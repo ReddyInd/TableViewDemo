793ad8523de36f1ffedf2fc267e66495d4d83c28
ReddyInd <goalsr@Goalsrs-MacBook-Pro.local>
2015-06-08 16:28:52 +0530

    Revert "This is for testing"
    This reverts commit 92203524150d3c669775165bac76f7aebbbf01cc.

--- TableViewDemo/ViewController.swift
+++ TableViewDemo/ViewController.swift
@@ -4,25 +4,14 @@
 //
 //  Created by Goalsr on 05/06/15.
 //  Copyright (c) 2015 Goalsr. All rights reserved.
+//
 
 import UIKit
 
+class ViewController: UIViewController {
 
-
-class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
-    
-    @IBOutlet weak var tableview: UITableView!
-    
-    var arrayItems = ["First","Second","Third","Fourth","Fifth"]
-    
-     override func viewDidLoad() {
-        
+    override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        tableview.registerClass(UITableViewCell.self, forCellReuseIdentifier:"cell")
-        
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -30,39 +19,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-       func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        
-        return arrayItems.count;
-        
-    }
-  
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-        
-    var cell:UITableViewCell = self.tableview.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
-        
-        cell.textLabel?.text = arrayItems[indexPath.row]
-        return cell
-        
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
-        
-        if indexPath.row == 0{
-            
-            
-        }
-        
-        
-        
-        
-    }
-    
-    
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 50.0;
-    }
-    
-    
+
+
 }
 
